24d03428fbe741343842ecc65d354e2e939b45cc
cromerofajar <cromerofajar@nautilus7.danielcastelao.org>
2019-02-18 11:36:47 +0100

    Boletin22 arreglado2

--- src/boletin22/Buzon.java
+++ src/boletin22/Buzon.java
@@ -11,19 +11,13 @@ import utilidades.PedirDatos;
 public class Buzon {
     public void Correo(ArrayList<Correos> cor){
         String correo=PedirDatos.texto("Introduzca el mensaje del correo a añadir");
-        boolean leido;
-        int lei=PedirDatos.entero("Introduzca 1 si el correo a sido leido, 0 si no:");
-        if (lei==1){
-            leido=true;
-        }
-        else{
-            leido=false;
-        }
+        boolean leido=false;
         Correos corre=new Correos(correo,leido);
         cor.add(corre);
     }
-    public void numeroDeCorreos(ArrayList<Correos> cor){
-        JOptionPane.showMessageDialog(null,"Tiene "+cor.size()+ " correos");
+    public int numeroDeCorreos(ArrayList<Correos> cor){
+        int numero=cor.size();
+        return numero;
     }
     public boolean porLeer(ArrayList<Correos> cor){
         for(Correos corre:cor){
@@ -33,15 +27,34 @@ public class Buzon {
         }
         return false;
     }
-    public void mostrarNoLeidoPrimero(ArrayList<Correos> cor){
-        for(Correos corre:cor){
-            if(corre.getLeido()==false){
-                System.out.println(corre);
+//    public String mostrarNoLeidoPrimero(ArrayList<Correos> cor){
+//        String mensaje;
+//        for(Correos corre:cor){
+//            if(corre.getLeido()==false){
+//                //if(!corre.getLeido()){ seria la otra opcion y automaticamente se ejecuta si es true
+//                mensaje=corre.getMensaje();
+//                corre.setLeido(true);
+//                return mensaje;
+//            }
+//        }
+//        return "Ninguno por leer";
+//    }
+//          SIN LLAMAR A porLeer.
+    
+    public String mostrarNoLeidoPrimero(ArrayList<Correos>cor){
+        boolean most=porLeer(cor);
+        String mensaje;
+        for(Correos corre: cor){
+            if(most){
+                mensaje=corre.getMensaje();
                 corre.setLeido(true);
-                break;
+                return mensaje;
             }
         }
+        return "Ninguno por leer";
     }
+    
+    
     public void mostrar(ArrayList<Correos> cor,int posicion){
         System.out.println(cor.get(posicion-1));
         Correos comprobacion=cor.get(posicion-1);
